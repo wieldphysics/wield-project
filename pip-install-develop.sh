263e5d0a12fb8b3363f795b97d1d6617a283e85f
Lee McCuller <Lee.McCuller@gmail.com>
2021-10-12 18:38:09 -0400

    filling out codes

---
+++ pip-install-develop.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#these are ordered specifically for the dependency resolution
+
+pip install -e wavestate-pytest/
+pip install -e wavestate-bunch/
+pip install -e wavestate-utilities/
+
+pip install -e wavestate-AAA/
+pip install -e wavestate-control/
+pip install -e wavestate-iirrational/
+pip install -e wavestate-quantum/
+
+pip install -e wavestate-devel/
+
+pip install -e wavestate-doc/
+pip install -e wavestate-model/
+pip install -e wavestate-test/
