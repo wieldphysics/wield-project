be20c04299bda876d4efeb16ebbf310e2949e56a
Lee McCuller <Lee.McCuller@gmail.com>
2023-10-27 16:30:35 -0700

    script improvement

--- git_check_uptodate.sh
+++ git_check_uptodate.sh
@@ -6,5 +6,20 @@ SHAUPS=$(git rev-parse @{u})
 if [[ $SHAHEAD == $SHAUPS ]]; then
     exit 0
 else
-    exit 1
+    behind_count=$(git rev-list --count HEAD..@{u})
+    ahead_count=$(git rev-list --count @{u}..HEAD)
+
+    if [[ $behind_count > 0 ]]; then
+        if [[ $ahead_count > 0 ]]; then
+            echo "diverged: " "$(basename $(pwd))"
+        else
+            echo "Not up to date: " "$(basename $(pwd))"
+        fi
+        exit 1
+    else
+        if [[ $ahead_count > 0 ]]; then
+            echo "ahead: " "$(basename $(pwd))"
+        fi
+        exit 0
+    fi
 fi
