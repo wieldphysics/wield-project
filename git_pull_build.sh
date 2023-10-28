8c0c12c3bbd8309f39a847b73d8e154c2aae3033
Lee McCuller <Lee.McCuller@gmail.com>
2023-10-27 17:47:34 -0700

    fixed the pull build

--- git_pull_build.sh
+++ git_pull_build.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 
 if [ ! -f up-to-date-flag.txt ]; then
-    git pull
-    git submodule foreach 'git pull'
-    ./pip-install-develop.sh --no-dependencies
     if [ ! -f currently-clean-flag.txt ]; then
+        git pull
+        git submodule foreach 'git pull'
+        ./pip-install-develop.sh --no-dependencies
         pushd wield-doc
         make fullclean
         popd
+        touch currently-clean-flag.txt
     fi
     pushd wield-doc
     make complete && touch ../up-to-date-flag.txt
